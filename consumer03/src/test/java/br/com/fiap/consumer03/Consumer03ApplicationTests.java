b12c7686f6ec3715c7d2ee0af37f56a8717fa3e5
Logon Aluno <logonaluno@local.com>
2020-01-29 22:15:24 -0300

    Criação do Consumer03

---
+++ consumer03/src/test/java/br/com/fiap/consumer03/Consumer03ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.consumer03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Consumer03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
